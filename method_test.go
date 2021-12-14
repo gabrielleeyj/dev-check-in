d78b94f29b36bf75f6f197af969adedb99d7d541
gabrielleeyj <gabrielleeyj@outlook.com>
2021-12-14 22:31:41 +0800

    second test to check if able to obtain child paths

--- method_test.go
+++ method_test.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"io/fs"
 	"os"
+	"path/filepath"
 	"testing"
 )
 
 var (
-	root  = "./mock"
-	files []os.DirEntry
-	err   error
+	root     = "./mock"
+	files    []os.DirEntry
+	err      error
+	findList []string
 )
 
 // Checks the directories available from root
@@ -20,5 +23,33 @@ func TestReadDirectory(t *testing.T) {
 	}
 	for _, file := range files {
 		println(file.Name())
+	}
+}
+
+// Able to check all possible paths of a directory
+// Originally use Walk but the latest 1.6 documentation recommended WalkDir which is more efficient
+func TestNestedDirectoryRead(t *testing.T) {
+	err := filepath.WalkDir(root, func(path string, info fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// checks if it is a directory
+		if info.IsDir() {
+			return nil
+		}
+
+		// appends full path of files to findList
+		findList = append(findList, path)
+		return nil
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	// check to see if the paths are appended
+	for _, path := range findList {
+		println(path)
 	}
 }
