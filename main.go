7a8893419232be01a29ea645a741bd9be57978f6
gabrielleeyj <gabrielleeyj@outlook.com>
2021-12-14 22:36:07 +0800

    import methods to main

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
 package main
+
+func main() {
+
+	// test directory
+	var root = "./mock"
+
+	// run method to get list of directory
+	dir, err := readDirectory(root)
+	if err != nil {
+		println(err)
+	}
+
+	// traverse through the list of directory and read the file
+	for _, path := range dir {
+		data, err := readChecker(path)
+		if err != nil {
+			println(err)
+		}
+
+		// if readChecker returns valid append to verified slice
+		println(data)
+	}
+}
