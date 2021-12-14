272d31db54fba582c65f01019991d86a89bf4c3f
gabrielleeyj <gabrielleeyj@outlook.com>
2021-12-14 22:35:56 +0800

    create methods from tests

---
+++ method.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"io/fs"
+	"os"
+	"path/filepath"
+)
+
+func readDirectory(root string) ([]string, error) {
+	var findList []string
+	err := filepath.WalkDir(root, func(path string, info fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+		findList = append(findList, path)
+		return nil
+	})
+	return findList, err
+}
+
+func readChecker(path string) (string, error) {
+	data, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(data)
+	line := 1
+	for scanner.Scan() {
+		if bytes.Contains(scanner.Bytes(), []byte("TODO")) {
+			return path, nil
+		}
+		line++
+	}
+
+	err = scanner.Err()
+	if err != nil {
+		panic(err)
+	}
+	return "", err
+}
