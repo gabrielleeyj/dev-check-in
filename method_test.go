809b174eb3913b1dffe3897aff1fe83c78399db3
gabrielleeyj <gabrielleeyj@outlook.com>
2021-12-14 22:25:04 +0800

    first test using std lib to read and return directory

--- method_test.go
+++ method_test.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"os"
+	"testing"
+)
+
+var (
+	root  = "./mock"
+	files []os.DirEntry
+	err   error
+)
+
+func TestReadDirectory(t *testing.T) {
+	files, err = os.ReadDir(root)
+	if err != nil {
+		panic(err)
+	}
+	for _, file := range files {
+		println(file.Name())
+	}
+}
